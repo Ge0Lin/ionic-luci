391866b03d391cfe7e7aa546cd2105fbbc8eed0f
Lars Boegild Thomsen <lth@netcompartner.com>
2015-03-06 09:26:08 +0800

    Hmmm - trying simple approach

---
+++ applications/luci-app-modbusgw/luasrc/model/cbi/modbusgw/index.lua
@@ -0,0 +1,18 @@
+-- Copyright 2015 Bright Things UN Ltd. <lth@bright-things.com>
+
+local fs = require "nixio.fs"
+
+local f = SimpleForm("modbusgw", "Modbus Gateway", "Explanation here")
+local o = f:field(Value, "_custom")
+
+o.template = "cbi/tvalue"
+o.rows = 20
+
+function o.cfgvalue(self, section) 
+	return.fs.readfile("/etc/modbusgw.json")
+end
+
+function o.write(self, section, value)
+	value = value:gsub("\r\n?", "\n")
+	fs.writefile("/etc/modbusgw.json")
+end
