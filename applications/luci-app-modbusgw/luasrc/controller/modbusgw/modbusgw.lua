88cea5b1facc992a85d36ba4724e28c4347ad21d
Lars Boegild Thomsen <lth@netcompartner.com>
2015-05-07 10:41:49 +0800

    Updated luci app

--- applications/luci-app-modbusgw/luasrc/controller/modbusgw/modbusgw.lua
+++ applications/luci-app-modbusgw/luasrc/controller/modbusgw/modbusgw.lua
@@ -6,10 +6,9 @@ module("luci.controller.modbusgw.modbusgw", package.seeall)
 
 function index() 
 
-	entry({"admin", "modbusgw"}, firstchild(), "Modbus Gateway", 30).dependent=false
-	entry({"admin", "modbusgw", "devices"}, cbi("modbusgw/devices"), "Devices", 1)
-	entry({"admin", "modbusgw", "definitions"}, cbi("modbusgw/defs"), "Device Definitions", 2)
+	entry({"admin", "modbusgw"}, firstchild(), translate("Modbus Gateway"), 30).dependent=false
+	entry({"admin", "modbusgw", "devices"}, cbi("modbusgw/devs"), translate("Devices"), 1)
+	entry({"admin", "modbusgw", "definitions"}, cbi("modbusgw/defs"), translate("Device Definitions"), 2)
 	entry({"admin", "modbusgw", "registers"}, cbi("modbusgw/regs"), nil).leaf=true
-
 
 end
--- applications/luci-app-modbusgw/luasrc/model/cbi/modbusgw/devs.lua
+++ applications/luci-app-modbusgw/luasrc/model/cbi/modbusgw/devs.lua
@@ -5,24 +5,52 @@
 local ds = require "luci.dispatcher"
 
 m = Map("modbusgw", translate("Modbus Gateway - Device definitions"), 
-	translate("The following is a list of all Modbus device types \
-                   currently supported by this Modbus Gateway.  To add a \
-                   new device, please define it here, or check \
-                   <a href='#'>here</a> if any pre-defined devices are \
-                   available."))
+	translate("The following defines the ports and the Modbus \
+                   devices used by the Modbus Gateway."))
 
-s = m:section(TypedSection, "definition", translate("Device Definitions"))
-s.template = "cbi/tblsection"
-s.addremove = true
-s.anonymous = false
-s.sortable = false
-s.extedit = ds.build_url("admin/modbusgw/registers", "%s")
+ports = m:section(TypedSection, "ports", translate("Ports"))
+ports.template = "cbi/tblsection"
+ports.addremove = true
+ports.anonymous = true
+ports.sortable = false
 
-ma = s:option(Value, "manufacturer", translate("Manufactuer"))
+portname = ports:option(Value, "name", translate("Name"))
+portdev = ports:option(ListValue, "serialport", translate("Serial Port"))
+portdev:value("/dev/ttyUSB0", "/dev/ttyUSB0")
+portspeed = ports:option(ListValue, "bitrate", translate("Serial Speed"))
+portspeed:value("115200", "115200")
+portspeed:value("57600", "57600")
+portspeed:value("38400", "38400")
+portspeed:value("19200", "19200")
+portspeed:value("9600", "9600")
+portspeed:value("4800", "4800")
+portspeed:value("2400", "2400")
+portparity = ports:option(ListValue, "parity", translate("Parity"))
+portparity:value("N", "None")
+portparity:value("E", "Even")
+portparity:value("O", "Odd")
+portparity:value("M", "Mark")
+portparity:value("S", "Space")
+portdelay = ports:option(Value, "delay", translate("Delay"))
 
-mo = s:option(Value, "model", translate("Model"))
 
-de = s:option(Value, "description", translate("Description"))
+devices = m:section(TypedSection, "devices", translate("Devices"))
+devices.template = "cbi/tblsection"
+devices.addremove = true
+devices.anonymous = true
+devices.sortable = false
+
+devicename = devices:option(Value, "name", translate("Name"))
+devicename.size=10
+devicedescription = devices:option(Value, "description", translate("Description"))
+deviceaddress = devices:option(Value, "address", translate("Address"))
+deviceaddress.size = 3
+devicepriority = devices:option(Value, "priority", translate("Priority"))
+devicepriority.size = 3
+devicepriority = devices:option(Value, "device", translate("Device"))
+devicedebug = devices:option(ListValue, "debug", translate("Debug"))
+devicedebug:value("false", translate("False"))
+devicedebug:value("true", translate("True"))
 
 return m
 
