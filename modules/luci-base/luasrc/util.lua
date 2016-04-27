748014e88a8453064594c603468dd53800387cac
Lars Boegild Thomsen <lth@netcompartner.com>
2016-04-27 10:42:28 +0200

    Hmmmm

--- modules/luci-base/luasrc/util.lua
+++ modules/luci-base/luasrc/util.lua
@@ -640,20 +640,6 @@ function checklib(fullpathexe, wantedlib)
 	local fs = require "nixio.fs"
 	local haveldd = fs.access('/usr/bin/ldd')
 	if not haveldd then
-<<<<<<< HEAD
-		return -1
-	end
-	local libs = exec("/usr/bin/ldd " .. fullpathexe)
-	if not libs then
-		return 0
-	end
-	for k, v in ipairs(split(libs)) do
-		if v:find(wantedlib) then
-			return 1
-		end
-	end
-	return 0
-=======
 		return false
 	end
 	local libs = exec("/usr/bin/ldd " .. fullpathexe)
@@ -666,7 +652,6 @@ function checklib(fullpathexe, wantedlib)
 		end
 	end
 	return false
->>>>>>> openwrt
 end
 
 --
